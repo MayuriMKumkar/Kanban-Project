47841add9330d45cb6e6d8a72dcce26b73c1b113
Mayuri Kumkar <mayurikumkar178@gmail.com>
2024-10-02 01:40:15 +0530

    Updated quicksell-KanbanBoard with new changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { useSelector } from "react-redux";
 import { ElementIcon } from './ElementIcon';
 
@@ -18,6 +18,7 @@ export const Card = ({ id, title, tag, name, users }) => {
     const userIds = name?.split("-")[1] - 1;
     const ticketIds = id?.split("-")[1] - 1;
     const val = users[userIds];
+    
     // Generate initial letters for the user
     const initial = val ? val.name
         .split(" ")
@@ -25,34 +26,35 @@ export const Card = ({ id, title, tag, name, users }) => {
         .join("") : "U";
     const ticketStatus = tickets[ticketIds]?.status;
 
-
     return (
         // Card Component
         <div className="card flex-gap-10" style={{ gap: '5px' }}>
             <div className="flex-sb">
-                {groupBy == "priority" &&
+                {groupBy === "priority" &&
                     <ElementIcon element={ticketStatus} />
                 }
                 <span style={{ color: "grey" }}>{id}</span>
-                {/* Initial to be used here if user is false*/}
+                {/* Initial to be used here if user is false */}
                 {
                     !user && (
                         <div style={{ position: "relative", width: "30px", height: "30px" }}>
-                            <img style={{ width: "100%", height: "100%", borderRadius: "50%" }} src={`https://ui-avatars.com/api/?name=${initial}&size=100&background=random&color=fff&rounded=true`}
-                                alt="User" />
+                            <img style={{ width: "100%", height: "100%", borderRadius: "50%" }} 
+                                 src={`https://ui-avatars.com/api/?name=${initial}&size=100&background=random&color=fff&rounded=true`}
+                                 alt="User" />
                         </div>
                     )
                 }
-
             </div>
             <div style={{ fontWeight: 200 }} >
                 <p>{title}</p>
             </div>
             <div className="cardTags">
                 {/* Display priority icon for non-priority grouping */}
-                {groupBy != "priority" && <div className="tags">
-                    <ElementIcon element={arr[tickets[ticketIds]?.priority]} />
-                </div>}
+                {groupBy !== "priority" && 
+                    <div className="tags">
+                        <ElementIcon element={arr[tickets[ticketIds]?.priority]} />
+                    </div>
+                }
                 {
                     tag?.map((elem, index) => {
                         return <div key={index} className="tags" style={{ color: "grey" }}> <span>•</span> {elem}</div>
